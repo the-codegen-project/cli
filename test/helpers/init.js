7afd0c1ceffc3e0cd706e52647a73451e3e00231
jonas lagoni <jonas-lt@live.dk>
2024-04-26 14:32:36 +0200

    feat: update cli implementation

--- test/helpers/init.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-file unicorn/prefer-module
-const path = require('node:path');
-process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json');
-process.env.NODE_ENV = 'development';
-
-global.oclif = global.oclif || {};
-global.oclif.columns = 80;
-
-require('node:events').EventEmitter.defaultMaxListeners = 30;
