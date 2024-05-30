13853618377134f1de43921a90625cbc3b3e6c82
Jonas Lagoni <jonas-lt@live.dk>
2024-05-30 13:23:37 +0200

    test: fix generate tests

--- test/commands/generate.spec.ts
+++ test/commands/generate.spec.ts
@@ -3,9 +3,10 @@ import { runCommand } from '@oclif/test';
 const CONFIG_MJS = path.resolve(__dirname, '../configs/config.js');
 
 describe('generate', () => {
-  it('shows user email when logged in', async () => {
-    const root = path.resolve(__dirname, '../../')
-    const {stdout, stderr, error, result} = await runCommand<{name: string}>(['generate', CONFIG_MJS]);
-    expect(stdout).toEqual('');
+  it('should be able generate simple configuration', async () => {
+    const {stdout, stderr, error} = await runCommand<{name: string}>(['generate', CONFIG_MJS]);
+    expect(error).toBeUndefined();
+    expect(stderr).toEqual('');
+    expect(stdout).not.toEqual('');
   });
 });
