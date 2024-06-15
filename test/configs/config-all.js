ead0b72d44222fbc77fe7179ad06ebf2eb3f8f5d
Jonas Lagoni <jonas-lt@live.dk>
2024-06-15 10:20:19 +0200

    fix test and implementation

---
+++ test/configs/config-all.js
@@ -0,0 +1,32 @@
+/** @type {import("@the-codegen-project/cli").TheCodegenConfiguration} TheCodegenConfiguration **/
+export default {
+	inputType: 'asyncapi',
+	inputPath: 'asyncapi.json',
+	generators: [
+		{
+			preset: 'payloads',
+			outputPath: './src/__gen__/',
+			serializationType: 'json', 
+			language: 'typescript'
+		},
+		{
+			preset: 'payloads',
+			outputPath: './src/__gen__/',
+			serializationType: 'json', 
+			packageName: 'test',
+			language: 'java'
+		},
+		{
+			preset: 'parameters',
+			outputPath: './src/__gen__/',
+			serializationType: 'json', 
+			language: 'typescript'
+		},
+		{
+			preset: 'channels',
+			outputPath: './src/__gen__/', 
+			language: 'typescript',
+			protocols: ['nats']
+		}
+	]
+};
