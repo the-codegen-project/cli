6b640b62a3cea44f62aa21e99da10396d3a8e927
Jonas Lagoni <jonas-lt@live.dk>
2024-06-22 11:49:36 +0200

    add implementation, test and docs

---
+++ src/codegen/generators/typescript/channels/protocols/nats/coreSubscribe.ts
@@ -0,0 +1,85 @@
+/* eslint-disable no-nested-ternary */
+import {SingleFunctionRenderType} from '../../../../../types';
+import {pascalCase, unwrap} from '../../../utils';
+import {ConstrainedMetaModel, ConstrainedObjectModel} from '@asyncapi/modelina';
+
+export function renderCoreSubscribe({
+  topic,
+  message,
+  channelParameters,
+  functionName = `subscribeTo${pascalCase(topic)}`
+}: {
+  topic: string;
+  message: ConstrainedMetaModel;
+  channelParameters: ConstrainedObjectModel | undefined;
+  functionName?: string;
+}): SingleFunctionRenderType {
+  const addressToUse = channelParameters
+    ? `parameters.getChannelWithParameters('${topic}')`
+    : topic;
+
+  const callbackFunctionParameters = [
+    { parameter: 'err?: Error', jsDoc: ' * @param err if any error occurred this will be sat' },
+    { parameter: `msg?: ${message.type}`, jsDoc: ' * @param msg that was received' },
+    ...(channelParameters ? [{ parameter: `parameters?: ${channelParameters.type}`, jsDoc: ' * @param parameters that was received in the topic' }] : []),
+  ];
+
+  const functionParameters = [
+    { parameter: `onDataCallback: (${callbackFunctionParameters.map(param => param.parameter).join(', ')}) => void`, jsDoc: ` * @param {${functionName}Callback} onDataCallback to call when messages are received` },
+    ...(channelParameters ? [{ parameter: `parameters: ${channelParameters.type}`, jsDoc: ' * @param parameters for topic substitution' }] : []),
+    { parameter: 'nc: Nats.NatsConnection', jsDoc: ' * @param nc the NATS client to subscribe through' },
+    { parameter: 'codec: any = Nats.JSONCodec()', jsDoc: ' * @param codec the serialization codec to use while receiving the message' },
+    { parameter: 'options?: Nats.SubscriptionOptions', jsDoc: ' * @param options when setting up the subscription' }
+  ];
+
+  const whenReceivingMessage = channelParameters
+    ? (message.type === 'null' 
+        ? `onDataCallback(undefined, null, parameters, msg);`
+        : `let receivedData: any = codec.decode(msg.data);
+onDataCallback(undefined, ${message.type}.unmarshal(receivedData), parameters, msg);`)
+    : (message.type === 'null'
+        ? `onDataCallback(undefined, null, msg);`
+        : `let receivedData: any = codec.decode(msg.data);
+onDataCallback(undefined, ${message.type}.unmarshal(receivedData), msg);`);
+
+  const jsDocParameters = functionParameters.map(param => param.jsDoc).join('\n');
+  const callbackJsDocParameters = callbackFunctionParameters.map(param => param.jsDoc).join('\n');
+
+  const code = `/**
+ * Callback for when receiving messages
+ *
+ * @callback ${functionName}Callback
+ ${callbackJsDocParameters}
+ */
+
+/**
+ * Core subscription for \`${addressToUse}\`
+ * 
+ ${jsDocParameters}
+ */
+export function ${functionName}(
+  ${functionParameters.map(param => param.parameter).join(', ')}
+): Promise<Nats.Subscription> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const subscription = await nc.subscribe(${addressToUse}, options);
+
+      (async () => {
+        for await (const msg of subscription) {
+          ${channelParameters ? unwrap(topic, channelParameters) : ''}
+          ${whenReceivingMessage}
+        }
+      })();
+      resolve(subscription);
+    } catch (e: any) {
+      reject(e);
+    }
+  });
+}`;
+
+  return {
+    code,
+    functionName,
+    dependencies: [`import * as Nats from 'nats';`]
+  };
+}
