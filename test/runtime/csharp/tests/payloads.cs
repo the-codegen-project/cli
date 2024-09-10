0385f782a79b336bca7a6e5e91a429e95c35867b
jonaslagoni <jonas-lt@live.dk>
2024-09-10 12:03:32 +0200

    fix runtimes

--- test/runtime/csharp/tests/payloads.cs
+++ test/runtime/csharp/tests/payloads.cs
@@ -1,83 +1,81 @@
-using NUnit.Framework;
 using System.Text.Json;
+using The.Codegen.Project;
 
-namespace The.Codegen.Project
+namespace csharp.tests;
+
+public class PayloadTestingJson
 {
-    [TestFixture]
-    public class PayloadTestingJson
+    [Test]
+    public void ShouldBeSerializableModel()
     {
-        [Test]
-        public void ShouldBeSerializableModel ()
+        var model = new The.Codegen.Project.Json.UserSignedUpPayload
         {
-            var model = new Json.UserSignedUp
-            {
-                DisplayName = "displayNameTest",
-                Email = "emailTest"
-            };
-            var json = JsonSerializer.Serialize(model);
-            var expectedJson = "{\"display_name\":\"displayNameTest\",\"email\":\"emailTest\"}";
-            Assert.AreEqual(json, expectedJson);
-        }
-        [Test]
-        public void ShouldBeDeserializeToModel()
-        {
-            var json = "{\"display_name\":\"displayNameTest\",\"email\":\"emailTest\"}";
-            var model = Json.UserSignedUp.Deserialize(json);
-            Assert.AreEqual(model.DisplayName, "displayNameTest");
-            Assert.AreEqual(model.Email, "emailTest");
-        }
-        [Test]
-        public void ShouldBeSerializableModelInRoundtrip()
-        {
-            var model = new Json.UserSignedUp
-            {
-                DisplayName = "displayNameTest",
-                Email = "emailTest"
-            };
-            var json = JsonSerializer.Serialize(model);
-
-            var deserializeOptions = new JsonSerializerOptions();
-            deserializeOptions.Converters.Add(new Json.UserSignedUpConverter());
-            var newModel = JsonSerializer.Deserialize<Json.UserSignedUp> (json, deserializeOptions);
-            var newJson = JsonSerializer.Serialize(newModel);
-            Assert.AreEqual(json, newJson);
-        }
+            DisplayName = "displayNameTest",
+            Email = "emailTest"
+        };
+        var json = JsonSerializer.Serialize(model);
+        var expectedJson = "{\"display_name\":\"displayNameTest\",\"email\":\"emailTest\"}";
+        Assert.That(expectedJson, Is.EqualTo(json));
     }
-    [TestFixture]
-    public class PayloadTestingNewtonsoft
+    [Test]
+    public void ShouldBeDeserializeToModel()
     {
-        [Test]
-        public void ShouldBeSerializableModel()
+        var json = "{\"display_name\":\"displayNameTest\",\"email\":\"emailTest\"}";
+        var model = The.Codegen.Project.Json.UserSignedUpPayload.Deserialize(json);
+        Assert.That("displayNameTest", Is.EqualTo(model.DisplayName));
+        Assert.That("emailTest", Is.EqualTo(model.Email));
+    }
+    [Test]
+    public void ShouldBeSerializableModelInRoundtrip()
+    {
+        var model = new The.Codegen.Project.Json.UserSignedUpPayload
         {
-            var model = new Newtonsoft.UserSignedUp
-            {
-                DisplayName = "displayNameTest",
-                Email = "emailTest"
-            };
-            var json = model.Serialize();
-            var expectedJson = "{\"display_name\":\"displayNameTest\",\"email\":\"emailTest\"}";
-            Assert.AreEqual(json, expectedJson);
-        }
-        [Test]
-        public void ShouldBeDeserializeToModel()
+            DisplayName = "displayNameTest",
+            Email = "emailTest"
+        };
+        var json = JsonSerializer.Serialize(model);
+
+        var deserializeOptions = new JsonSerializerOptions();
+        deserializeOptions.Converters.Add(new The.Codegen.Project.Json.UserSignedUpPayloadConverter());
+        var newModel = JsonSerializer.Deserialize<The.Codegen.Project.Json.UserSignedUpPayload>(json, deserializeOptions);
+        var newJson = JsonSerializer.Serialize(newModel);
+        Assert.That(newJson, Is.EqualTo(json));
+    }
+}
+[TestFixture]
+public class PayloadTestingNewtonsoft
+{
+    [Test]
+    public void ShouldBeSerializableModel()
+    {
+        var model = new The.Codegen.Project.Newtonsoft.UserSignedUpPayload
         {
-            var json = "{\"display_name\":\"displayNameTest\",\"email\":\"emailTest\"}";
-            var model = Newtonsoft.UserSignedUp.Deserialize(json);
-            Assert.AreEqual(model.DisplayName, "displayNameTest");
-            Assert.AreEqual(model.Email, "emailTest");
-        }
-        [Test]
-        public void ShouldBeSerializableModelInRoundtrip()
+            DisplayName = "displayNameTest",
+            Email = "emailTest"
+        };
+        var json = model.Serialize();
+        var expectedJson = "{\"display_name\":\"displayNameTest\",\"email\":\"emailTest\"}";
+        Assert.That(expectedJson, Is.EqualTo(json));
+    }
+    [Test]
+    public void ShouldBeDeserializeToModel()
+    {
+        var json = "{\"display_name\":\"displayNameTest\",\"email\":\"emailTest\"}";
+        var model = The.Codegen.Project.Newtonsoft.UserSignedUpPayload.Deserialize(json);
+        Assert.That("displayNameTest", Is.EqualTo(model.DisplayName));
+        Assert.That("emailTest", Is.EqualTo(model.Email));
+    }
+    [Test]
+    public void ShouldBeSerializableModelInRoundtrip()
+    {
+        var model = new The.Codegen.Project.Newtonsoft.UserSignedUpPayload
         {
-            var model = new Newtonsoft.UserSignedUp
-            {
-                DisplayName = "displayNameTest",
-                Email = "emailTest"
-            };
-            var json = model.Serialize();
-            var newModel = Newtonsoft.UserSignedUp.Deserialize(json);
-            var newJson = newModel.Serialize();
-            Assert.AreEqual(json, newJson);
-        }
+            DisplayName = "displayNameTest",
+            Email = "emailTest"
+        };
+        var json = model.Serialize();
+        var newModel = The.Codegen.Project.Newtonsoft.UserSignedUpPayload.Deserialize(json);
+        var newJson = newModel.Serialize();
+        Assert.That(newJson, Is.EqualTo(json));
     }
 }
