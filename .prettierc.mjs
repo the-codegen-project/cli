6c8757b8316221f7eb07de72762459a707eac92d
Lagoni <andreastlagoni@gmail.com>
2024-10-01 15:50:20 +0200

    lint issue fix

--- .prettierc.mjs
+++ .prettierc.mjs
@@ -2,8 +2,8 @@ import companyPrettierConfig from "@oclif/prettier-config";
 
 export default {
   ...companyPrettierConfig,
-  "semi": true,
-  "trailingComma": "none",
-  "singleQuote": true,
-  "printWidth": 80
+  semi: true,
+  trailingComma: "none",
+  singleQuote: true,
+  printWidth: 80,
 };
--- src/codegen/generators/typescript/utils.ts
+++ src/codegen/generators/typescript/utils.ts
@@ -5,9 +5,9 @@ import {
   NO_RESERVED_KEYWORDS,
   typeScriptDefaultModelNameConstraints,
   typeScriptDefaultPropertyKeyConstraints,
-  TypeScriptOptions,
-} from "@asyncapi/modelina";
-import { DeepPartial } from "../../utils";
+  TypeScriptOptions
+} from '@asyncapi/modelina';
+import {DeepPartial} from '../../utils';
 
 /**
  * Component which contains the parameter unwrapping functionality.
@@ -32,7 +32,7 @@ export function unwrap(
 ) {
   // Nothing to unwrap if no parameters are used
   if (Object.keys(channelParameters.properties).length === 0) {
-    return "";
+    return '';
   }
 
   const parameterReplacement = Object.values(channelParameters.properties).map(
@@ -54,19 +54,19 @@ export function unwrap(
     }
   );
 
-  let topicWithWildcardGroup = channelName.replaceAll(/\//g, "\\/");
+  let topicWithWildcardGroup = channelName.replaceAll(/\//g, '\\/');
   topicWithWildcardGroup = topicWithWildcardGroup.replaceAll(
     /{[^}]+}/g,
-    "([^.]*)"
+    '([^.]*)'
   );
   const regexMatch = `/^${topicWithWildcardGroup}$/`;
 
   return `const regex = ${regexMatch};
-const parameters = new ${channelParameters.name}({${parameterInitializer.join(", ")}});
+const parameters = new ${channelParameters.name}({${parameterInitializer.join(', ')}});
 const match = msg.subject.match(regex);
 
 if (match) {
-  ${parameterReplacement.join("\n")}
+  ${parameterReplacement.join('\n')}
 } else {
   console.error(\`Was not able to retrieve parameters, ignoring message. Subject was: \${msg.subject}\`);
   return;
@@ -81,16 +81,16 @@ if (match) {
  */
 export function castToTsType(jsonSchemaType: string, variableToCast: string) {
   switch (jsonSchemaType.toLowerCase()) {
-    case "string": {
+    case 'string': {
       return `"" + ${variableToCast}`;
     }
 
-    case "integer":
-    case "number": {
+    case 'integer':
+    case 'number': {
       return `Number(${variableToCast})`;
     }
 
-    case "boolean": {
+    case 'boolean': {
       return `Boolean(${variableToCast})`;
     }
 
@@ -109,7 +109,7 @@ export function castToTsType(jsonSchemaType: string, variableToCast: string) {
 export function realizeParametersForChannelWithoutType(
   parameters: ConstrainedObjectModel
 ) {
-  let returnString = "";
+  let returnString = '';
   for (const paramName in Object.keys(parameters.properties)) {
     returnString += `${paramName},`;
   }
@@ -130,7 +130,7 @@ export function realizeParametersForChannel(
   channelParameters: ConstrainedObjectModel,
   required = true
 ) {
-  let returnString = "";
+  let returnString = '';
   for (const parameter of Object.values(channelParameters.properties)) {
     returnString += `${realizeParameterForChannelWithType(parameter.propertyName, parameter.property.type, required)},`;
   }
@@ -154,7 +154,7 @@ function realizeParameterForChannelWithType(
   parameterType: string,
   required = true
 ) {
-  const requiredType = required ? "" : "?";
+  const requiredType = required ? '' : '?';
   return `${parameterName}${requiredType}: ${parameterType})}`;
 }
 
@@ -170,7 +170,7 @@ export function renderJSDocParameters(
     .map((paramName) => {
       return `* @param ${paramName} parameter to use in topic`;
     })
-    .join("\n");
+    .join('\n');
 }
 
 /**
@@ -181,7 +181,7 @@ export function realizeChannelName(
   parameters?: ConstrainedObjectModel
 ) {
   let returnString = `\`${channelName}\``;
-  returnString = returnString.replaceAll("/", ".");
+  returnString = returnString.replaceAll('/', '.');
   for (const paramName in parameters) {
     returnString = returnString.replace(`{${paramName}}`, `\${${paramName}}`);
   }
@@ -207,68 +207,68 @@ export function pascalCase(value: string) {
 }
 
 export const RESERVED_TYPESCRIPT_KEYWORDS = [
-  "break",
-  "case",
-  "catch",
-  "class",
-  "const",
-  "continue",
-  "debugger",
-  "default",
-  "delete",
-  "do",
-  "else",
-  "enum",
-  "export",
-  "extends",
-  "false",
-  "finally",
-  "for",
-  "function",
-  "if",
-  "import",
-  "in",
-  "instanceof",
-  "new",
-  "null",
-  "return",
-  "super",
-  "switch",
-  "this",
-  "throw",
-  "true",
-  "try",
-  "typeof",
-  "var",
-  "void",
-  "while",
-  "with",
-  "any",
-  "boolean",
-  "constructor",
-  "declare",
-  "get",
-  "module",
-  "require",
-  "number",
-  "set",
-  "string",
-  "symbol",
-  "type",
-  "from",
-  "of",
+  'break',
+  'case',
+  'catch',
+  'class',
+  'const',
+  'continue',
+  'debugger',
+  'default',
+  'delete',
+  'do',
+  'else',
+  'enum',
+  'export',
+  'extends',
+  'false',
+  'finally',
+  'for',
+  'function',
+  'if',
+  'import',
+  'in',
+  'instanceof',
+  'new',
+  'null',
+  'return',
+  'super',
+  'switch',
+  'this',
+  'throw',
+  'true',
+  'try',
+  'typeof',
+  'var',
+  'void',
+  'while',
+  'with',
+  'any',
+  'boolean',
+  'constructor',
+  'declare',
+  'get',
+  'module',
+  'require',
+  'number',
+  'set',
+  'string',
+  'symbol',
+  'type',
+  'from',
+  'of',
   // Strict mode reserved words
-  "arguments",
-  "as",
-  "implements",
-  "interface",
-  "let",
-  "package",
-  "private",
-  "protected",
-  "public",
-  "static",
-  "yield",
+  'arguments',
+  'as',
+  'implements',
+  'interface',
+  'let',
+  'package',
+  'private',
+  'protected',
+  'public',
+  'static',
+  'yield'
 ];
 
 export const defaultCodegenTypescriptModelinaOptions: DeepPartial<TypeScriptOptions> =
@@ -281,8 +281,8 @@ export const defaultCodegenTypescriptModelinaOptions: DeepPartial<TypeScriptOpti
         },
         NO_SPECIAL_CHAR: (name) => {
           // Looks nicer with no special cases at all
-          return name.replace(/\W/g, " ");
-        },
+          return name.replace(/\W/g, ' ');
+        }
       }),
       propertyKey: typeScriptDefaultPropertyKeyConstraints({
         NO_RESERVED_KEYWORDS: (value) => {
@@ -291,12 +291,12 @@ export const defaultCodegenTypescriptModelinaOptions: DeepPartial<TypeScriptOpti
             checkForReservedKeyword(
               word,
               RESERVED_TYPESCRIPT_KEYWORDS.filter((filteredValue) => {
-                return filteredValue !== "type";
+                return filteredValue !== 'type';
               }),
               true
             )
           );
-        },
-      }),
-    },
+        }
+      })
+    }
   };
