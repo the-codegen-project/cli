234f32f5b532baaaf432b6370bad589a64b2573b
Jonas Lagoni <jonas-lt@live.dk>
2024-10-08 17:46:05 +0200

    chore(release): v0.19.1 (#125)

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,24 @@ export {run} from '@oclif/core';
 
 export {loadConfigFile} from './codegen/configuration-manager';
 export {loadAsyncapi} from './codegen/inputs/asyncapi';
-export {RunGeneratorContext, SupportedLanguages, PresetTypes, TheCodegenConfiguration, TheCodegenConfigurationInternal, zodAsyncAPICodegenConfiguration, zodAsyncAPIGenerators, zodAsyncAPITypeScriptGenerators, zodTheCodegenConfiguration, Generators, GenericCodegenContext, GenericGeneratorOptions,LoadArgument,ParameterRenderType, PayloadRenderType, SingleFunctionRenderType} from './codegen/types';
+export {
+  RunGeneratorContext,
+  SupportedLanguages,
+  PresetTypes,
+  TheCodegenConfiguration,
+  TheCodegenConfigurationInternal,
+  zodAsyncAPICodegenConfiguration,
+  zodAsyncAPIGenerators,
+  zodAsyncAPITypeScriptGenerators,
+  zodTheCodegenConfiguration,
+  Generators,
+  GenericCodegenContext,
+  GenericGeneratorOptions,
+  LoadArgument,
+  ParameterRenderType,
+  PayloadRenderType,
+  SingleFunctionRenderType
+} from './codegen/types';
 export {runGenerators} from './codegen/index';
 export {
   defaultTypeScriptChannelsGenerator,
