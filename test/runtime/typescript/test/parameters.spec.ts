a06a90c51b54020e0bd780e6f4489ebc2f3f5073
jonas lagoni <jonas-lt@live.dk>
2024-10-19 15:46:04 +0200

    update runtime test

--- test/runtime/typescript/test/parameters.spec.ts
+++ test/runtime/typescript/test/parameters.spec.ts
@@ -2,10 +2,16 @@ import { UserSignedupParameters } from '../src/parameters/UserSignedupParameters
 
 describe('parameters', () => {
   const testObject = new UserSignedupParameters({
-    myParameter: 'parameter'
+    myParameter: 'parameter',
+    enumParameter: 'asyncapi'
+  });
+  test('getChannelWithParameters should return correct channel', () => {
+    const channelWithParameters = testObject.getChannelWithParameters('user.signedup.{my_parameter}.{enum_parameter}');
+    expect(channelWithParameters).toEqual(`user.signedup.${testObject.myParameter}.${testObject.myParameter}`);
   });
   test('be able to get correct channel', () => {
-    const channelWithParameters = testObject.getChannelWithParameters('user.signedup.{my_parameter}');
-    expect(channelWithParameters).toEqual(`user.signedup.${testObject.myParameter}`);
+    const parameter = UserSignedupParameters.createFromChannel('user.signedup.test_my_parameter.asyncapi', 'user.signedup.{my_parameter}.{enum_parameter}', /^user.signedup.([^.]*).([^.]*)$/);
+    expect(parameter.enumParameter).toEqual('asyncapi');
+    expect(parameter.myParameter).toEqual('test_my_parameter');
   });
 });
