cd8dfac912d52d368fd089b52f2c1e7201750272
Jonas Lagoni <jonas-lt@live.dk>
2025-03-21 20:51:02 +0100

    chore: refactor nats and mqtt and add post (#194)

--- src/codegen/generators/typescript/channels/protocols/mqtt/index.ts
+++ src/codegen/generators/typescript/channels/protocols/mqtt/index.ts
@@ -11,6 +11,8 @@ import {getMessageTypeAndModule} from '../../utils';
 import {renderPublish} from './publish';
 import {shouldRenderFunctionType, getFunctionTypeMappingFromAsyncAPI} from '../../asyncapi';
 import { ChannelInterface } from '@asyncapi/parser';
+import { SingleFunctionRenderType } from '../../../../../types';
+import { ConstrainedObjectModel } from '@asyncapi/modelina';
 
 export {renderPublish};
 
@@ -21,83 +23,116 @@ export async function generateMqttChannels(
   externalProtocolFunctionInformation: Record<string, renderedFunctionType[]>,
   dependencies: string[]
 ) {
-  const {generator, payloads, parameter, topic} = context;
-  let functionTypeMapping = generator.functionTypeMapping[channel.id()];
+  const {generator, parameter, topic} = context;
   const ignoreOperation = !generator.asyncapiGenerateForOperations;
-  let mqttContext: RenderRegularParameters = {
+  const mqttContext: RenderRegularParameters = {
     channelParameters: parameter,
     topic,
     subName: context.subName,
     messageType: ''
   };
-  const renders = [];
+  let renders = [];
   const operations = channel.operations().all();
   if (operations.length > 0 && !ignoreOperation) {
-    for (const operation of operations) {
-      functionTypeMapping = getFunctionTypeMappingFromAsyncAPI(operation) ?? functionTypeMapping;
-      const payloadId = findOperationId(operation, channel);
-      const payload = payloads.operationModels[payloadId];
-      if (payload === undefined) {
-        throw new Error(
-          `Could not find payload for ${payloadId} for channel typescript generator ${JSON.stringify(payloads.operationModels, null, 4)}`
-        );
-      }
-      const {messageModule, messageType} = getMessageTypeAndModule(payload);
-      mqttContext = {
-        ...mqttContext,
-        messageType,
-        messageModule,
-        subName: findNameFromOperation(operation, channel)
-      };
-
-      const action = operation.action();
-      if (
-        shouldRenderFunctionType(
-          functionTypeMapping,
-          ChannelFunctionTypes.MQTT_PUBLISH,
-          action,
-          generator.asyncapiReverseOperations
-        )
-      ) {
-        renders.push(renderPublish(mqttContext));
-      }
-    }
+    renders = generateForOperations(context, channel, mqttContext);
   } else {
-    functionTypeMapping = getFunctionTypeMappingFromAsyncAPI(channel) ?? functionTypeMapping;
-    const payload = payloads.channelModels[channel.id()];
-    if (payload === undefined) {
-      throw new Error(
-        `Could not find payload for ${channel.id()} for channel typescript generator`
-      );
-    }
-    const {messageModule, messageType} = getMessageTypeAndModule(payload);
-    mqttContext = {...mqttContext, messageType, messageModule};
-    if (
-      shouldRenderFunctionType(
-        functionTypeMapping,
-        ChannelFunctionTypes.MQTT_PUBLISH,
-        'send',
-        generator.asyncapiReverseOperations
-      )
-    ) {
-      renders.push(renderPublish(mqttContext));
-    }
+    renders = generateForChannels(context, channel, mqttContext);
   }
+  addRendersToExternal(renders, protocolCodeFunctions, externalProtocolFunctionInformation, dependencies, parameter);
+}
+
+function addRendersToExternal(
+  renders: SingleFunctionRenderType[],
+  protocolCodeFunctions: Record<string, string[]>,
+  externalProtocolFunctionInformation: Record<string, renderedFunctionType[]>,
+  dependencies: string[],
+  parameter?: ConstrainedObjectModel
+) {
   protocolCodeFunctions['mqtt'].push(...renders.map((value) => value.code));
 
   externalProtocolFunctionInformation['mqtt'].push(
-    ...renders.map((value) => {
-      return {
-        functionType: value.functionType,
-        functionName: value.functionName,
-        messageType: value.messageType,
-        replyType: value.replyType,
-        parameterType: parameter?.type
-      } as renderedFunctionType;
-    })
+    ...renders.map((value) => ({
+      functionType: value.functionType,
+      functionName: value.functionName,
+      messageType: value.messageType,
+      replyType: value.replyType,
+      parameterType: parameter?.type
+    }))
   );
   const renderedDependencies = renders
     .map((value) => value.dependencies)
     .flat(Infinity);
   dependencies.push(...(new Set(renderedDependencies) as any));
+}
+
+function generateForOperations(
+  context: TypeScriptChannelsGeneratorContext,
+  channel: ChannelInterface,
+  mqttContext: RenderRegularParameters
+): SingleFunctionRenderType[] {
+  const renders: SingleFunctionRenderType[] = [];
+  const { generator, payloads } = context;
+  const functionTypeMapping = generator.functionTypeMapping[channel.id()];
+
+  for (const operation of channel.operations().all()) {
+    const updatedFunctionTypeMapping = getFunctionTypeMappingFromAsyncAPI(operation) ?? functionTypeMapping;
+    const payloadId = findOperationId(operation, channel);
+    const payload = payloads.operationModels[payloadId];
+    if (payload === undefined) {
+      throw new Error(
+        `Could not find payload for ${payloadId} for channel typescript generator ${JSON.stringify(payloads.operationModels, null, 4)}`
+      );
+    }
+    const {messageModule, messageType} = getMessageTypeAndModule(payload);
+    const updatedContext = {
+      ...mqttContext,
+      messageType,
+      messageModule,
+      subName: findNameFromOperation(operation, channel)
+    };
+
+    const action = operation.action();
+    if (
+      shouldRenderFunctionType(
+        updatedFunctionTypeMapping,
+        ChannelFunctionTypes.MQTT_PUBLISH,
+        action,
+        generator.asyncapiReverseOperations
+      )
+    ) {
+      renders.push(renderPublish(updatedContext));
+    }
+  }
+  return renders;
+}
+
+function generateForChannels(
+  context: TypeScriptChannelsGeneratorContext,
+  channel: ChannelInterface,
+  mqttContext: RenderRegularParameters
+): SingleFunctionRenderType[] {
+  const renders: SingleFunctionRenderType[] = [];
+  const { generator, payloads } = context;
+  const functionTypeMapping = generator.functionTypeMapping[channel.id()];
+  
+  const updatedFunctionTypeMapping = getFunctionTypeMappingFromAsyncAPI(channel) ?? functionTypeMapping;
+  const payload = payloads.channelModels[channel.id()];
+  if (payload === undefined) {
+    throw new Error(
+      `Could not find payload for ${channel.id()} for channel typescript generator`
+    );
+  }
+  const {messageModule, messageType} = getMessageTypeAndModule(payload);
+  const updatedContext = {...mqttContext, messageType, messageModule};
+  if (
+    shouldRenderFunctionType(
+      updatedFunctionTypeMapping,
+      ChannelFunctionTypes.MQTT_PUBLISH,
+      'send',
+      generator.asyncapiReverseOperations
+    )
+  ) {
+    renders.push(renderPublish(updatedContext));
+  }
+  return renders;
 }
