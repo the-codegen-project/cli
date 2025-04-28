f02781b7f2cc49e516213391af1bb9ddb89e8946
Jonas Lagoni <jonas-lt@live.dk>
2025-04-28 21:55:04 +0200

    feat: add validation to kafka (#204)

--- src/codegen/generators/typescript/channels/protocols/kafka/subscribe.ts
+++ src/codegen/generators/typescript/channels/protocols/kafka/subscribe.ts
@@ -4,6 +4,7 @@ import {ChannelFunctionTypes} from '../..';
 import {SingleFunctionRenderType} from '../../../../../types';
 import {findRegexFromChannel, pascalCase} from '../../../utils';
 import {RenderRegularParameters} from '../../types';
+import {getValidationFunctions} from '../../utils';
 
 export function renderSubscribe({
   topic,
@@ -11,13 +12,25 @@ export function renderSubscribe({
   messageModule,
   channelParameters,
   subName = pascalCase(topic),
-  functionName = `consumeFrom${subName}`
+  functionName = `consumeFrom${subName}`,
+  payloadGenerator
 }: RenderRegularParameters): SingleFunctionRenderType {
+  const includeValidation = payloadGenerator.generator.includeValidation;
   const addressToUse = channelParameters
     ? `parameters.getChannelWithParameters('${topic}')`
     : `'${topic}'`;
-  const messageUnmarshalling = `${messageModule ?? messageType}.unmarshal(message.value?.toString()!)`;
+  const messageUnmarshalling = `${messageModule ?? messageType}.unmarshal(receivedData)`;
   messageType = messageModule ? `${messageModule}.${messageType}` : messageType;
+
+  const {potentialValidatorCreation, potentialValidationFunction} =
+    getValidationFunctions({
+      includeValidation,
+      messageModule,
+      messageType,
+      onValidationFail: channelParameters
+        ? `return onDataCallback(new Error('Invalid message payload received', {cause: errors}), undefined, parameters, kafkaMessage);`
+        : `return onDataCallback(new Error('Invalid message payload received', {cause: errors}), undefined, kafkaMessage);`
+    });
 
   const callbackFunctionParameters = [
     {
@@ -63,6 +76,11 @@ export function renderSubscribe({
       parameter:
         "options: {fromBeginning: boolean, groupId: string} = {fromBeginning: true, groupId: ''}",
       jsDoc: ' * @param options when setting up the subscription'
+    },
+    {
+      parameter: 'skipMessageValidation: boolean = false',
+      jsDoc:
+        ' * @param skipMessageValidation turn off runtime validation of outgoing messages'
     }
   ];
   let whenReceivingMessage = '';
@@ -70,14 +88,16 @@ export function renderSubscribe({
     if (messageType === 'null') {
       whenReceivingMessage = `onDataCallback(undefined, null, parameters, kafkaMessage);`;
     } else {
-      whenReceivingMessage = `const callbackData = ${messageUnmarshalling};
+      whenReceivingMessage = `${potentialValidationFunction}
+const callbackData = ${messageUnmarshalling};
 onDataCallback(undefined, callbackData, parameters, kafkaMessage);`;
     }
   } else if (messageType === 'null') {
     whenReceivingMessage = `onDataCallback(undefined, null, kafkaMessage);`;
   } else {
-    whenReceivingMessage = `const callbackData = ${messageUnmarshalling};
-  onDataCallback(undefined, callbackData, kafkaMessage);`;
+    whenReceivingMessage = `${potentialValidationFunction}
+const callbackData = ${messageUnmarshalling};
+onDataCallback(undefined, callbackData, kafkaMessage);`;
   }
   const jsDocParameters = functionParameters
     .map((param) => param.jsDoc)
@@ -104,15 +124,17 @@ ${functionName}: (
   return new Promise(async (resolve, reject) => {
     try {
       if(!options.groupId) {
-        reject('No group ID provided');
+        return reject('No group ID provided');
       }
       const consumer = kafka.consumer({ groupId: options.groupId });
 
+      ${potentialValidatorCreation}
       await consumer.connect();
       await consumer.subscribe({ topic: ${addressToUse}, fromBeginning: options.fromBeginning });
       await consumer.run({
         eachMessage: async (kafkaMessage: Kafka.EachMessagePayload) => {
           const { topic, message } = kafkaMessage;
+          const receivedData = message.value?.toString()!;
           ${channelParameters ? `const parameters = ${channelParameters.type}.createFromChannel(topic, '${topic}', ${findRegexFromChannel(topic)});` : ''}
           ${whenReceivingMessage}
         }
--- test/runtime/typescript/test/channels/kafka.spec.ts
+++ test/runtime/typescript/test/channels/kafka.spec.ts
@@ -2,7 +2,7 @@ import { Protocols } from '../../src/channels';
 const { kafka } = Protocols;
 const { 
   produceToNoParameter, consumeFromNoParameter, consumeFromReceiveUserSignedup, produceToSendUserSignedup } = kafka;
-import { Kafka, EachMessagePayload } from 'kafkajs';
+import { Kafka } from 'kafkajs';
 import { UserSignedupParameters } from '../../src/parameters/UserSignedupParameters';
 import { UserSignedUp } from '../../src/payloads/UserSignedUp';
 const kafkaClient = new Kafka({
@@ -10,51 +10,120 @@ const kafkaClient = new Kafka({
   brokers: ['localhost:9093'],
 });
 jest.setTimeout(10000);
+function createRandomString(length) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
 describe('kafka', () => {
   const testMessage = new UserSignedUp({displayName: 'test', email: 'test@test.dk'});
+  const invalidMessage = new UserSignedUp({displayName: 'test', email: '123'});
   const testParameters = new UserSignedupParameters({myParameter: 'test', enumParameter: 'asyncapi'});
+  const admin = kafkaClient.admin({retry: {retries: 5, initialRetryTime: 1000}});
+  beforeAll(async () => {
+    await admin.connect();
+    await admin.createTopics({
+      topics: [
+        { topic: 'user.signedup.test.asyncapi', numPartitions: 1 },
+        { topic: 'noparameters', numPartitions: 1 },
+      ],
+    });
+  });
+  beforeEach(async() => {
+    try {
+      await admin.deleteTopicRecords({
+        topic: 'user.signedup.test.asyncapi',
+        partitions: [{ partition: 0, offset: '-1' }],
+      });
+      await admin.deleteTopicRecords({
+        topic: 'noparameters',
+        partitions: [{ partition: 0, offset: '-1' }],
+      });
+    } catch (e) {}
+  });
+  afterAll(async () => {
+    await admin.disconnect();
+  });
   describe('channels', () => {
     describe('with parameters', () => {
+      it('should get error on incorrect message', () => {
+        // eslint-disable-next-line no-async-promise-executor
+        let consumer;
+        return new Promise<void>(async (resolve, reject) => {
+          consumer = await consumeFromReceiveUserSignedup(async (err, msg, parameters) => {
+            try {
+              expect(msg).toBeUndefined();
+              expect(err).toBeDefined();
+              expect(err?.message).toEqual('Invalid message payload received');
+              expect(err?.cause).toBeDefined();
+              expect(parameters?.myParameter).toEqual(testParameters.myParameter);
+              resolve();
+            } catch (error) {
+              reject(error);
+            }
+          }, new UserSignedupParameters({myParameter: 'test', enumParameter: 'asyncapi'}), kafkaClient, {fromBeginning: true, groupId: createRandomString(10)});
+          const producer = await produceToSendUserSignedup(invalidMessage, testParameters, kafkaClient);
+          await producer.disconnect();
+        }).finally(async () => {
+          if (consumer) {
+            await consumer.stop();
+            await consumer.disconnect();
+          }
+        });
+      });
       it('should be able to publish and consume', () => {
         // eslint-disable-next-line no-async-promise-executor
+        let consumer;
         return new Promise<void>(async (resolve, reject) => {
-          const consumer = await consumeFromReceiveUserSignedup(async (err, msg: UserSignedUp | undefined, parameters: UserSignedupParameters | undefined, kafkaMsg: EachMessagePayload | undefined) => {
+          consumer = await consumeFromReceiveUserSignedup(async (err, msg, parameters) => {
             try {
               expect(err).toBeUndefined();
               expect(msg?.marshal()).toEqual(testMessage.marshal());
               expect(parameters?.myParameter).toEqual(testParameters.myParameter);
-              await consumer.disconnect();
               resolve();
             } catch (error) {
               reject(error);
-              await consumer.disconnect();
             }
-          }, new UserSignedupParameters({myParameter: 'test', enumParameter: 'asyncapi'}), kafkaClient, {fromBeginning: true, groupId: 'testId1'});
+          }, new UserSignedupParameters({myParameter: 'test', enumParameter: 'asyncapi'}), kafkaClient, {fromBeginning: true, groupId: createRandomString(10)});
           const producer = await produceToSendUserSignedup(testMessage, testParameters, kafkaClient);
           await producer.disconnect();
+        }).finally(async () => {
+          if (consumer) {
+            await consumer.stop();
+            await consumer.disconnect();
+          }
         });
-      });
+      })
     });
     describe('without parameters', () => {
       it('should be able to publish and consume', () => {
         // eslint-disable-next-line no-async-promise-executor
+        let consumer;
         return new Promise<void>(async (resolve, reject) => {
-          const consumer = await consumeFromNoParameter(
+          consumer = await consumeFromNoParameter(
             async (err, msg) => {
               try {
                 expect(err).toBeUndefined();
                 expect(msg?.marshal()).toEqual(testMessage.marshal());
-                await consumer.disconnect();
                 resolve();
               } catch (error) {
                 reject(error);
               }
             }, 
             kafkaClient, 
-            {fromBeginning: true, groupId: 'testId2'}
+            {fromBeginning: true, groupId: createRandomString(10)}
           );
           const producer = await produceToNoParameter(testMessage, kafkaClient);
           await producer.disconnect();
+        }).finally(async () => {
+          if (consumer) {
+            await consumer.stop();
+            await consumer.disconnect();
+          }
         });
       });
     });
