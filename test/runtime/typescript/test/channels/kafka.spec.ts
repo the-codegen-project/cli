f336b8155bea1a2a87fd8a2675639ad3cfa9c6f5
jonaslagoni <jonas-lt@live.dk>
2025-04-28 21:46:41 +0200

    fix tests

--- test/runtime/typescript/test/channels/kafka.spec.ts
+++ test/runtime/typescript/test/channels/kafka.spec.ts
@@ -23,27 +23,27 @@ describe('kafka', () => {
   const testMessage = new UserSignedUp({displayName: 'test', email: 'test@test.dk'});
   const invalidMessage = new UserSignedUp({displayName: 'test', email: '123'});
   const testParameters = new UserSignedupParameters({myParameter: 'test', enumParameter: 'asyncapi'});
-  const admin = kafkaClient.admin();
+  const admin = kafkaClient.admin({retry: {retries: 5, initialRetryTime: 1000}});
   beforeAll(async () => {
     await admin.connect();
     await admin.createTopics({
-      topics: [{
-        topic: 'user.signedup.test.asyncapi'
-      },{
-        topic: 'noparameters'
-      }],
-      waitForLeaders: true,
+      topics: [
+        { topic: 'user.signedup.test.asyncapi', numPartitions: 1 },
+        { topic: 'noparameters', numPartitions: 1 },
+      ],
     });
   });
-  afterEach(async() => {
-    await admin.deleteTopicRecords({
-      topic: 'user.signedup.test.asyncapi',
-      partitions: [{ partition: 0, offset: '-1' }],
-    });
-    await admin.deleteTopicRecords({
-      topic: 'noparameters',
-      partitions: [{ partition: 0, offset: '-1' }],
-    });
+  beforeEach(async() => {
+    try {
+      await admin.deleteTopicRecords({
+        topic: 'user.signedup.test.asyncapi',
+        partitions: [{ partition: 0, offset: '-1' }],
+      });
+      await admin.deleteTopicRecords({
+        topic: 'noparameters',
+        partitions: [{ partition: 0, offset: '-1' }],
+      });
+    } catch (e) {}
   });
   afterAll(async () => {
     await admin.disconnect();
@@ -52,30 +52,36 @@ describe('kafka', () => {
     describe('with parameters', () => {
       it('should get error on incorrect message', () => {
         // eslint-disable-next-line no-async-promise-executor
+        let consumer;
         return new Promise<void>(async (resolve, reject) => {
-          const consumer = await consumeFromReceiveUserSignedup(async (err, msg, parameters) => {
-            try {
-              expect(msg).toBeUndefined();
-              expect(err).toBeDefined();
-              expect(err?.message).toEqual('Invalid message payload received');
-              expect(err?.cause).toBeDefined();
-              expect(parameters?.myParameter).toEqual(testParameters.myParameter);
-              resolve();
-            } catch (error) {
-              reject(error);
-            } finally {
-              await consumer.stop();
-              await consumer.disconnect();
-            }
-          }, new UserSignedupParameters({myParameter: 'test', enumParameter: 'asyncapi'}), kafkaClient, {fromBeginning: true, groupId: createRandomString(10)});
-          const producer = await produceToSendUserSignedup(invalidMessage, testParameters, kafkaClient);
-          await producer.disconnect();
+          setTimeout(async () => {
+            consumer = await consumeFromReceiveUserSignedup(async (err, msg, parameters) => {
+              try {
+                expect(msg).toBeUndefined();
+                expect(err).toBeDefined();
+                expect(err?.message).toEqual('Invalid message payload received');
+                expect(err?.cause).toBeDefined();
+                expect(parameters?.myParameter).toEqual(testParameters.myParameter);
+                resolve();
+              } catch (error) {
+                reject(error);
+              }
+            }, new UserSignedupParameters({myParameter: 'test', enumParameter: 'asyncapi'}), kafkaClient, {fromBeginning: true, groupId: createRandomString(10)});
+            const producer = await produceToSendUserSignedup(invalidMessage, testParameters, kafkaClient);
+            await producer.disconnect();
+          }, 2000);
+        }).finally(async () => {
+          if (consumer) {
+            await consumer.stop();
+            await consumer.disconnect();
+          }
         });
       });
       it('should be able to publish and consume', () => {
         // eslint-disable-next-line no-async-promise-executor
+        let consumer;
         return new Promise<void>(async (resolve, reject) => {
-          const consumer = await consumeFromReceiveUserSignedup(async (err, msg, parameters) => {
+          consumer = await consumeFromReceiveUserSignedup(async (err, msg, parameters) => {
             try {
               expect(err).toBeUndefined();
               expect(msg?.marshal()).toEqual(testMessage.marshal());
@@ -83,21 +89,24 @@ describe('kafka', () => {
               resolve();
             } catch (error) {
               reject(error);
-            } finally {
-              await consumer.stop();
-              await consumer.disconnect();
             }
           }, new UserSignedupParameters({myParameter: 'test', enumParameter: 'asyncapi'}), kafkaClient, {fromBeginning: true, groupId: createRandomString(10)});
           const producer = await produceToSendUserSignedup(testMessage, testParameters, kafkaClient);
           await producer.disconnect();
+        }).finally(async () => {
+          if (consumer) {
+            await consumer.stop();
+            await consumer.disconnect();
+          }
         });
-      });
+      })
     });
     describe('without parameters', () => {
       it('should be able to publish and consume', () => {
         // eslint-disable-next-line no-async-promise-executor
+        let consumer;
         return new Promise<void>(async (resolve, reject) => {
-          const consumer = await consumeFromNoParameter(
+          consumer = await consumeFromNoParameter(
             async (err, msg) => {
               try {
                 expect(err).toBeUndefined();
@@ -105,9 +114,6 @@ describe('kafka', () => {
                 resolve();
               } catch (error) {
                 reject(error);
-              } finally {
-                await consumer.stop();
-                await consumer.disconnect();
               }
             }, 
             kafkaClient, 
@@ -115,6 +121,11 @@ describe('kafka', () => {
           );
           const producer = await produceToNoParameter(testMessage, kafkaClient);
           await producer.disconnect();
+        }).finally(async () => {
+          if (consumer) {
+            await consumer.stop();
+            await consumer.disconnect();
+          }
         });
       });
     });
