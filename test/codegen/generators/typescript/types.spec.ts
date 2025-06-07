570e17c967ee76bc05ab72eb0a2277b2111e4206
jonaslagoni <jonas-lt@live.dk>
2025-06-07 10:52:41 +0200

    wip

--- test/codegen/generators/typescript/types.spec.ts
+++ test/codegen/generators/typescript/types.spec.ts
@@ -1,5 +1,6 @@
 import path from "node:path";
 import { loadAsyncapiDocument } from "../../../../src/codegen/inputs/asyncapi";
+import { loadOpenapiDocument } from "../../../../src/codegen/inputs/openapi/parser";
 import { generateTypescriptTypes } from "../../../../src/codegen/generators";
 
 describe('types', () => {
@@ -34,6 +35,57 @@ describe('types', () => {
         },
         inputType: 'asyncapi',
         asyncapiDocument: parsedAsyncAPIDocument,
+        dependencyOutputs: { }
+      });
+      expect(renderedContent.result).toMatchSnapshot();
+    });
+    it('should work with OpenAPI 2.0 inputs', async () => {
+      const parsedOpenAPIDocument = await loadOpenapiDocument(path.resolve(__dirname, '../../../configs/openapi-2.json'));
+      
+      const renderedContent = await generateTypescriptTypes({
+        generator: {
+          outputPath: path.resolve(__dirname, './output'),
+          preset: 'types',
+          language: 'typescript',
+          dependencies: [],
+          id: 'test'
+        },
+        inputType: 'openapi',
+        openapiDocument: parsedOpenAPIDocument,
+        dependencyOutputs: { }
+      });
+      expect(renderedContent.result).toMatchSnapshot();
+    });
+    it('should work with OpenAPI 3.0 inputs', async () => {
+      const parsedOpenAPIDocument = await loadOpenapiDocument(path.resolve(__dirname, '../../../configs/openapi-3.json'));
+      
+      const renderedContent = await generateTypescriptTypes({
+        generator: {
+          outputPath: path.resolve(__dirname, './output'),
+          preset: 'types',
+          language: 'typescript',
+          dependencies: [],
+          id: 'test'
+        },
+        inputType: 'openapi',
+        openapiDocument: parsedOpenAPIDocument,
+        dependencyOutputs: { }
+      });
+      expect(renderedContent.result).toMatchSnapshot();
+    });
+    it('should work with OpenAPI 3.1 inputs', async () => {
+      const parsedOpenAPIDocument = await loadOpenapiDocument(path.resolve(__dirname, '../../../configs/openapi-3_1.json'));
+      
+      const renderedContent = await generateTypescriptTypes({
+        generator: {
+          outputPath: path.resolve(__dirname, './output'),
+          preset: 'types',
+          language: 'typescript',
+          dependencies: [],
+          id: 'test'
+        },
+        inputType: 'openapi',
+        openapiDocument: parsedOpenAPIDocument,
         dependencyOutputs: { }
       });
       expect(renderedContent.result).toMatchSnapshot();
