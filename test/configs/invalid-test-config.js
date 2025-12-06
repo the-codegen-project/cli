654fc62cac1b4d51654711c5a258a0132f1c93b7
jonaslagoni <jonas-lt@live.dk>
2025-12-06 19:41:47 +0100

    add config

---
+++ test/configs/invalid-test-config.js
@@ -0,0 +1,11 @@
+
+        export default {
+          inputType: 'asyncapi',
+          inputPath: './non-existent-schema.yml',
+          generators: [
+            {
+              preset: 'invalid-preset-that-does-not-exist',
+              outputPath: './output'
+            }
+          ]
+        };
