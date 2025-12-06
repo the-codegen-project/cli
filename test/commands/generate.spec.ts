405b57400dbc30e4fd36ae269f68cc74d4038e42
jonaslagoni <jonas-lt@live.dk>
2025-12-06 20:27:25 +0100

    wi[

--- test/commands/generate.spec.ts
+++ test/commands/generate.spec.ts
@@ -3,13 +3,6 @@ import { runCommand } from '@oclif/test';
 const CONFIG_MJS = path.resolve(__dirname, '../configs/config.js');
 
 describe('generate', () => {
-  it('should be able generate simple configuration', async () => {
-    const {stdout, stderr, error} = await runCommand(`generate ${CONFIG_MJS}`);
-    expect(error).toBeUndefined();
-    expect(stderr).toEqual('');
-    expect(stdout).not.toEqual('');
-  });
-  
   it('should be able to generate hello world with custom presets', async () => {
     const {error} = await runCommand(`generate ${CONFIG_MJS}`);
     expect(error).toBeUndefined();
--- test/configs/config.js
+++ test/configs/config.js
@@ -1,7 +1,7 @@
 /** @type {import("@the-codegen-project/cli").TheCodegenConfiguration} TheCodegenConfiguration **/
 export default {
 	inputType: 'asyncapi',
-	inputPath: './asyncapi.yaml',
+	inputPath: 'asyncapi.json',
 	language: 'typescript',
 	generators: [
 		{
